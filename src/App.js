ffbf64521dc40492d76332f6d656a00f07a63d64
Muhammad Andika <74449013+akrn27@users.noreply.github.com>
2023-11-12 19:26:40 +0700

    install several dependecies from npm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Button } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/loginpage";
+import HomePage from "./pages/homepage";
 
 function App() {
   return (
-    <div>
-      <Button variant="contained">Hello world</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
