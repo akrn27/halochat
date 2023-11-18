9d9db63f2d02bd93762cd153db2119494c112de8
Muhammad Andika <74449013+akrn27@users.noreply.github.com>
2023-11-18 16:37:09 +0700

    added likes and replies feature

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,8 +5,10 @@ import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import { useDispatch } from "react-redux";
 import { getAllPosts } from "../features/auth/authSlice";
-import { Box, Typography } from "@mui/material";
-import {format, parse} from 'date-fns'
+import { Box, Button, Typography } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -36,27 +38,65 @@ const HomePage = () => {
   }, []);
 
   const getPosts = async () => {
-    const token = Cookies.get('user_token')
+    const token = Cookies.get("user_token");
     const config = {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
+        Authorization: `Bearer ${token}`,
+      },
+    };
 
-    const response = await dispatch(getAllPosts({config}))
-    console.log('res => ', response.payload.data.data)
-    setPosts(response.payload.data.data)
-  }
+    const response = await dispatch(getAllPosts({ config }));
+    console.log("res => ", response.payload.data.data);
+    setPosts(response.payload.data.data);
+  };
   useEffect(() => {
-    getPosts()
-  }, [])
+    getPosts();
+  }, []);
 
   return (
     <Layout>
       <Box>
-        {posts && posts?.map((post, i) => (
-          <Typography>@{post.user.name} {new Date(post.created_at).toLocaleDateString("en-US")}</Typography>
-        ))}
+        {posts &&
+          posts?.map((post, i) => (
+            <Box
+              sx={{
+                display: "flex",
+                gap: "10px",
+                marginBottom: "10px",
+                padding: "4px",
+              }}
+            >
+              <PersonIcon sx={{ fontSize: "50px" }} />
+              <Box>
+                <Box sx={{ display: "flex", gap: "4px" }}>
+                  <Typography sx={{ fontWeight: "800" }}>
+                    @{post.user.name}
+                  </Typography>
+                  <Typography sx={{ color: "#616161" }}>
+                    {new Date(post.created_at).toLocaleDateString("en-US")}
+                  </Typography>
+                </Box>
+                <Typography sx={{ fontWeight: "600" }}>
+                  {post.description}
+                </Typography>
+
+                <Box sx={{ display: "flex", gap: "3px", alignItems: "center" }}>
+                  {post.is_like_post ? (
+                    <ThumbUpAltIcon />
+                  ) : (
+                    <ThumbUpOffAltIcon />
+                  )}
+                  <Typography>{post.likes_count}</Typography>
+                  <Button
+                    variant="text"
+                    sx={{ color: "black", fontWeight: "600" }}
+                  >
+                    Reply
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          ))}
       </Box>
     </Layout>
   );
